de45bba04089184e605033e06d3d28c95dd3e8a2
JanaTartousy <tartousyjana@gmail.com>
2023-02-17 19:27:38 +0200

    step-5

---
+++ php-basics-01/Step_5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name = "Rust, the language of no bugs";
+$enrolled_students = 20;
+$price = 30.0;
+$on_discount = true; 
+
+echo "Course title: " . $course_name . "\n";
+echo "Enrolled Students: " . $enrolled_students . "\n";
+echo "Course price: " . $price . " \$ USD\n";
+echo "Course on discount: " . ($on_discount ? "Yes" : "No") . "\n";
+?>
