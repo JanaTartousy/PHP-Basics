f2918a207db4a346a062975b7a999ca4bb9e0a8d
JanaTartousy <tartousyjana@gmail.com>
2023-02-17 16:28:08 +0200

    step-3

---
+++ php-basics-01/Step_3.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Jana";
+$email = "tartousyjana@gmail.com";
+
+echo "My name is \"$name\" and my email is \"$email\".\n";
+
+$height = 15;
+$width = 10;
+$area = $height * $width;
+
+echo "The area is: $area";
+?>
