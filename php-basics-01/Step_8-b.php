5ce47c3fd9e0232c5bfde4990293eea607e72d03
JanaTartousy <tartousyjana@gmail.com>
2023-02-18 14:28:35 +0200

    step-8-b

---
+++ php-basics-01/Step_8-b.php
@@ -0,0 +1,15 @@
+<?php
+$items = [
+    ['balade', 'mazere3'],
+    ['Fresh', 'Taanayel'],
+    ['Tanoureen', 'Reem']
+];
+
+$egg_qty = 1;
+$water_pack_qty = 3;
+
+$eggs = $items[0][0];
+$water_pack = $items[2][1];
+
+echo "Hey Sir, Please I need {$egg_qty} pack of {$eggs} eggs and {$water_pack_qty} {$water_pack} Water Pack.";
+?>
