034b7fa8b29401692d827b7492e1efb4cf7b7c0d
JanaTartousy <tartousyjana@gmail.com>
2023-02-18 10:51:25 +0200

    step-7

---
+++ php-basics-01/Step_7.php
@@ -0,0 +1,10 @@
+<?php
+$birth_year = 2002; 
+$current_year = date("Y"); 
+
+if ($current_year - $birth_year > 18) {
+    echo "You can drive";
+} else {
+    echo "You're still a kid, go and play GTA";
+}
+?>
